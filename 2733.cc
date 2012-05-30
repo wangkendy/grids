582fa168502c1c07b794e9c05d9dc1abcc91772f
Kendy Wang <wkendy@gmail.com>
2012-05-31 00:52:33 +0800

    2733 Leap year

---
+++ 2733.cc
@@ -0,0 +1,21 @@
+/*
+ * source: http://poj.grids.cn/practice/2733
+ * tag: date and time
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int y;
+	cin >> y;
+	if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) {
+		cout << "Y" << endl;
+	} else {
+		cout << "N" << endl;
+	}
+	return 0;
+}
